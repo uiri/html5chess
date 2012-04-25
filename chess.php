ce19671b5ecd0424b8258f44427abab4091fa904
Uiri <uiri00@gmail.com>
2012-04-25 17:10:15 -0400

    Getting no where in particular

--- chess.php
+++ chess.php
@@ -3,21 +3,52 @@
 $conn = pg_connect("host=" . $host . " dbname=" . $dbname . " user=" . $user . " password=" . $password)
     or die("Could not connect: " . pg_last_error());
 
-$query = "SELECT * FROM games WHERE player1ip == " . $_SERVER['REMOTE_ADDR'] . " OR player2ip == " . $_SERVER['REMOTE_ADDR'];
+echo "Connected";
+
+$query = "SELECT * FROM games WHERE player1ip = '" . $_SERVER['REMOTE_ADDR'] . "' OR player2ip = '" . $_SERVER['REMOTE_ADDR'] . "';";
 $result = pg_query($query) or die("Query error: " . pg_last_error());
 
-header("Contet-type:application/json");
+if (!preg_match("/^[A-Za-z0-9 ]+$/", $_POST["player1"])) {
+    exit();
+}
 
-if (pg_fetch_result($result)) {
-    
-} else {
-    $query = "SELECT * FROM games WHERE player2 == NULL";
+if ($_POST["player2"]) {
+    if (!preg_match("/^[A-Za-z0-9 ]+$/", $_POST["player1"])) {
+	exit();
+    }
+}
+
+if (($_POST["white"] != "true" && $_POST["white"] != "true") || ($_POST["kingmovedwhite"] != "true" && $_POST["kingmovedwhite"] != "false") || ($_POST["kingmovedblack"] != "true" && $_POST["kingmovedblack"] != "false")) {
+    exit();
+}
+
+/*if (pg_fetch_result($result)) {
+
+//} else  */
+if (!pg_fetch_result($result)) {
+    echo "One";
+    $query = "SELECT * FROM games WHERE player2 = NULL;";
     $result = pg_query($query) or die("Query error: " . pg_last_error());
-    if (pg_fetch_result($result)) {
-	$query = "UPDATE games SET player2 = " . $_POST["player1"] . ", player2ip = " . $_SERVER["REMOTE_ADDR"] . "WHERE player1 == " . pg_fetch_assoc($result)['player1'] . " AND player2 == NULL";
+    $resassoc = pg_fetch_assoc($result);
+    if ($resassoc != NULL) {
+	echo "two";
+	$query = "UPDATE games SET player2 = " . $_POST["player1"] . ", player2ip = " . $_SERVER["REMOTE_ADDR"] . "WHERE player1 = " . $resassoc['player1'] . " AND player2 = NULL;";
 	$result = pg_query($query) or die("Query error: " . pg_last_error());
     } else {
-	//$query = 
+	echo "three";
+	$query = "INSERT INTO games (player1, white, kingmovedwhite, kingmovedblack, board, player1ip) VALUES ('" . $_POST["player1"] . "', 'true', 'false', 'false', '" . $_POST["board"] . "', '" . $_SERVER["REMOTE_ADDR"] . "');";
+	$result = pg_query($query) or die("Query error: " . pg_last_error());
+	$opponent = FALSE;
+	while (!$opponent) {
+	    $query = "SELECT * FROM games WHERE player1 = '" . $_POST["player1"] . "' AND player1ip = '" . $_SERVER["REMOTE_ADDR"] . "';";
+	    $result = pg_query($query) or die("Query error: " . pg_last_error());
+	    $resassoc = pg_fetch_assoc($result);
+	    if ($resassoc["player2"] != NULL) {
+		$opponent = TRUE;
+	    }
+	    sleep(1);
+	}
+	echo "{ 'player1' : '" . $resassoc['player1'] . "', 'player2' : '" . $resassoc['player2'] . "' }";
     }
 }
 
